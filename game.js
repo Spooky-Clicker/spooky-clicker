993b079097c4dd8b595bddaaf9930ee6c1da3ca3
jakecarroll84 <32414087+jakecarroll84@users.noreply.github.com>
2017-10-07 17:52:00 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-<font color="white">
+
 let score = 0;
 let cookieElement = document.getElementById('cookie');
 let scoreElement = document.getElementById('score');
@@ -7,4 +7,4 @@ cookieElement.addEventListener('click', function(){
     score++;
     scoreElement.textContent= score + "Spooky Clicker";
 });
-</font>
+
