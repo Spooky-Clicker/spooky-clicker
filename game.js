366939d34484e3f805656fa71295515e0d186e72
jakecarroll84 <32414087+jakecarroll84@users.noreply.github.com>
2017-10-07 14:48:24 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
+<font color="white">
 let score = 0;
 let cookieElement = document.getElementById('cookie');
 let scoreElement = document.getElementById('score');
 
 cookieElement.addEventListener('click', function(){
     score++;
-    scoreElement.textContent <font color="white">= score + "Spooky Clicker"</font>;
+    scoreElement.textContent= score + "Spooky Clicker";
 });
+</font>
